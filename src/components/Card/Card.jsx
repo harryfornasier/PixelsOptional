f6516e962e983a0191a4a77a19fa861fdc01ae40
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-11-24 13:39:35 +0000

    Add regex for date

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,22 +3,26 @@ import "./card.scss";
 import Lines from "../Lines/Lines";
 
 export default function Card(postProp) {
+  const timeStampPattern = /(\d{4})-(\d{2})-(\d{2})T.*$/;
   const post = postProp.post;
   return (
     <section className="card">
       <div className="card__header-container">
-        <Lines />
         <h3 className="card__title">{post.title}</h3>
         <Lines />
-        <h3 className="card__date"></h3>
+        <h3 className="card__date">
+          {post.created_at.replace(timeStampPattern, "$3/$2/$1")}
+        </h3>
       </div>
 
-      <Link to={`/post/${post.id}`}>
-        <img src={post.image_url} alt="" className="card__img" />
-      </Link>
+      <div className="card__image-container">
+        <Link to={`/post/${post.id}`}>
+          <img src={post.image_url} alt="" className="card__img" />
+        </Link>
+      </div>
 
       <div className="card__details-container">
-        <p className="card__likes"></p>
+        <p className="card__likes">{post.like}</p>
         <p className="card__comments"></p>
         <p className="card__camera"></p>
       </div>
