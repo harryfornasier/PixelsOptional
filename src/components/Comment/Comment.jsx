c6ecf0b168c9459eac4c88beaf5e06e66e1ee9d2
Harrison Fornasier <harrisonfornasier@gmail.com>
2024-11-27 18:41:06 +0000

    Update for author

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -4,7 +4,7 @@ export default function Comment({ comment }) {
   return (
     <section className="comment">
       <div className="comment__author-container">
-        <p className="comment__author">{comment.user_name}:</p>
+        <p className="comment__author">{comment.user_name && `${comment.user_name}:`}</p>
       </div>
       <p className="comment__text">
         {comment.comment ? comment.comment : "No one has posted a comment..."}
